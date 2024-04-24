7c2753218a0a87fa86594b16d60307665f85e6f4
Steve Djiala <inf3623>
2024-04-24 20:38:39 +0200

    add c++  first file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Das ist ein Programm, das eine Illustration von cout zeigt  !" << endl;
+    return 0;
+}
